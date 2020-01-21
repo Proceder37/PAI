0a63783a71e33d3d397fad938bfa50e009cd5b8d
Patryk Kaczor <51462170+Proceder37@users.noreply.github.com>
2020-01-22 00:08:47 +0100

    Add files via upload

---
+++ PAI/Views/index.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="Stylesheet" type="text/css" href="../Public/css/style.css" />
+    <title>WorkCounter</title>
+</head>
+<body>
+<div class="container">
+    <div class="logo">
+        <img src="../Public/img/logo.svg">
+        <img src="../Public/img/workcounter.svg">
+    </div>
+    <form action="zaloguj.php", method="post">
+        <input name="email" type="text" placeholder="email@email.com">
+        <br></br>
+        <input name="password" type="password" placeholder="password">
+        <br></br>
+        <button type="submit">CONTINUE</button>
+
+    </form>
+
+    <?php
+        if(isset($_SESSION['blad']))    echo $_SESSION['blad'];
+    ?>
+</div>
+</body>
+</html>
