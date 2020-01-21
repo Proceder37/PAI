87040da267f687fee02309cebde56b7c8a49f978
Patryk Kaczor <51462170+Proceder37@users.noreply.github.com>
2020-01-21 01:06:56 +0100

    Add files via upload

---
+++ PAI/Views/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="Stylesheet" type="text/css" href="../Public/css/style.css" />
+    <title>WorkCounter</title>
+</head>
+<body>
+<div class="container">
+    <div class="logo">
+        <img src="../Public/img/logo.svg">
+        <img src="../Public/img/workcounter.svg">
+    </div>
+    <form action="zaloguj.php", method="post">
+        <input name="email" type="text" placeholder="email@email.com">
+        <br></br>
+        <input name="password" type="password" placeholder="password">
+        <br></br>
+        <button type="submit">CONTINUE</button>
+        <br></br>
+        <button type="submit">CREAT AN ACCOUNT</button>
+        <br></br>
+    </form>
+</div>
+</body>
+</html>
---
+++ PAI/Views/start.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="Stylesheet" type="text/css" href="../Public/css/style.css" />
+    <title>WorkCounter</title>
+</head>
+<body>
+<div class="container">
+    <div class="logo">
+        <img src="../Public/img/start1.svg">
+    </div>
+    <form>
+
+    </form>
+
+
+</div>
+</body>
+</html>
